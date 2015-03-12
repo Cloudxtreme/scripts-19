ef8ae9e8e15f399cc524f53a8f1f9b176e953806
Mario Sanchez Prada <mario@mariospr.org>
2015-03-12 11:56:06 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+# Scripts
+
+Collection of scripts I wrote to scratch some itches and not necesarily useful to anyone else, but that I would still like to keep under version control
+
+## License
+
+These scripts are licensed under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+See the COPYING file for the full version of the GNU GPLv2 license
+
---
+++ qemu2vbox.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#
+# Converts QEMU raws images to Virtual Box's vdi format
+#
+# Copyright (C) 2012 Mario Sanchez Prada.
+# Authors: Mario Sanchez Prada <mario@mariospr.org>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 (or later)
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# This script updates your WebKit local respository in a faster way
+# than just using git-svn rebase by fetching changes from the git
+# mirror to the associated remote repository first, then updating the
+# references in your local branch tracking the SVN repository.
+
+function quit ()
+{
+    local EXIT_CODE=0
+
+    # return exit code, if present
+    [ $# -gt 0 ] && EXIT_CODE=$1
+
+    # show descriptive message, if present
+    [ $# -gt 1 ] && echo "" && echo "[ERROR]: ${2}" && echo ""
+
+    exit ${EXIT_CODE}
+}
+
+function usage ()
+{
+    echo "Usage: qemu2vbox <origin file> [<destination file>]"
+    echo ""
+}
+
+# MAIN BODY
+
+if [ $# -eq 0 ] || [ $# -gt 2 ] || [ "${1}" == "help" ] || [ "${1}" == "usage" ]; then
+    # show usage
+    usage
+    exit 0
+fi
+
+# check requirements
+which VBoxManage > /dev/null 2>&1 || \
+    quit 1 "Command 'VBoxManage' not found. Haven't you installed VirtualBox yet?"
+
+# get paths and names for origin file
+ORIGIN_PATH="$1"
+if [[ "$1" != /* ]]; then
+    # path is relative
+    ORIGIN_PATH="$(pwd)/$(dirname $1)/$(basename $1)"
+fi
+shift 1
+
+# check path and names for (optional) destination file
+if [ $# -gt 0 ]; then
+    DESTINATION_PATH="$1"
+
+    if [[ "$1" != /* ]]; then
+        # path is relative
+        DESTINATION_PATH="$(pwd)/$(dirname $1)/$(basename $1)"
+    fi
+else
+    DESTINATION_PATH="${ORIGIN_PATH}.vdi"
+fi
+shift 1
+
+echo "Converting the QEMU image to Virtual Box's format..."
+VBoxManage convertdd "${ORIGIN_PATH}" "${DESTINATION_PATH}" || \
+    quit 1 "A problem has ocurred while converting the image"
+echo "[OK]: Image converted"
+echo
+
+echo "Compacting Virtual Box's image..."
+VBoxManage modifyvdi "${DESTINATION_PATH}" compact || \
+    quit 1 "A problem has ocurred while compacting the result image"
+echo "[OK]: Image compacted"
+echo
+
+echo "Process finished!"
+echo
+
+exit 0
